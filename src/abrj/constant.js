1cf55377c27e925d7e3feb798e45cb3d77941518
limadavidnascimento@gmail.com <limadavidnascimento@gmail.com>
2024-10-11 01:01:06 -0300

    feat: abrj e protel

--- src/abrj/constant.js
+++ src/abrj/constant.js
@@ -9,23 +9,23 @@ export const AWS_KEY_ID = ""
 
 
 export const ENVVAR = {
-    DB_HOST: "24.144.96.26",
-    DB_NAME: "alugueseguro_admimo",
-    DB_USER: "alugueseguro_admim",
-    DB_PASSWORD: "xGriWP!Ut#Hd1v9K",
+    DB_HOST: "",
+    DB_NAME: "",
+    DB_USER: "",
+    DB_PASSWORD: "",
 
     RPA_BCF_URL: "https://app.bcfadm.com.br/Index.aspx",
-    RPA_BCF_LOGIN: "11518759700",
-    RPA_BCF_PASSWORD: "Seletto311",
+    RPA_BCF_LOGIN: "",
+    RPA_BCF_PASSWORD: "",
 
     RPA_SUPERLOGICA_LOGIN: "administrativo1@administramosimoveis.com.br",
-    RPA_SUPERLOGICA_PASSWORD: "Seletto311",
+    RPA_SUPERLOGICA_PASSWORD: "",
 
-    RPA_APSA_LOGIN: "26581108553",
-    RPA_APSA_PASSWORD: "Seletto311",
+    RPA_APSA_LOGIN: "",
+    RPA_APSA_PASSWORD: "",
 
-    RPA_PROTEL_LOGIN: "4337021004",
-    RPA_PROTEL_PASSWORD: "7",
+    RPA_PROTEL_LOGIN: "",
+    RPA_PROTEL_PASSWORD: "",
 }
 
 
