42f18347d0b0f6302a32062e20d396d5dfe86b3a
zz-dev <105506965+Dev-Zz@users.noreply.github.com>
2024-10-31 22:47:52 -0300

    ajuste valor vortex

--- bots/src/vortex/download_page.py
+++ bots/src/vortex/download_page.py
@@ -51,6 +51,7 @@ class VortexDownloadPage:
 
                 valor_e = boleto.find_element(*self.valor_locator)
                 vlr_boleto = valor_e.text.strip()
+                vlr_boleto = float(vlr_boleto.replace('.', '').replace(',', '.'))
 
                 linha_digitavel_e = boleto.find_element(*self.linha_digitavel_locator)
                 linha_digitavel = linha_digitavel_e.text.replace(".", "").replace(" ", "").replace("-", "").strip()
